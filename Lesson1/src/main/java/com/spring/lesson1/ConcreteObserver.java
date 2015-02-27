9f84c76b4b359a2d48946077889b0f234d290bef
Andrew Sidun <asidun@mail.ru>
2015-02-27 10:53:39 +0200

    Remove using ContextLocator

--- Lesson1/src/main/java/com/spring/lesson1/ConcreteObserver.java
+++ Lesson1/src/main/java/com/spring/lesson1/ConcreteObserver.java
@@ -10,55 +10,23 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 
 import com.spring.lesson1.message.Message;
 
-public class ConcreteObserver implements Observer, BeanPostProcessor, BeanNameAware, BeanFactoryAware {
+public class ConcreteObserver implements Observer{
 
 	private Integer id;
 
 	public ConcreteObserver() {
 		final Random random = new Random();
 		id = random.nextInt();
-		System.out.println("Constructor. id=" + id);
+		System.out.println(" ConcreteObserver constructor. id=" + id);
 	}
 	
 	public Integer getId(){
 		return id;
 	}
 
-	@Override
 	public void notify(Message message) {
 		System.out.println("Observer id = " + id + ". "
 				+ message.getDescription() + ": " + message.getContent());
-	}
-
-	@Override
-	public Object postProcessAfterInitialization(Object arg0, String arg1)
-			throws BeansException {		
-		System.out.println("postProcessAfterInitialization. " + arg0.getClass().getName());
-		return arg0;
-	}
-
-	@Override
-	public Object postProcessBeforeInitialization(Object arg0, String arg1)
-			throws BeansException {
-		System.out.println("postProcessBeforeInitialization");
-		return arg0;
-	}
-
-	@Override
-	public void setBeanName(String arg0) {
-		System.out.println("setBeanName. " + arg0);
-		
-	}
-	
-	public void init(){
-		System.out.println("Init method");
-	}
-
-	@Override
-	public void setBeanFactory(BeanFactory arg0) throws BeansException {
-		ConcreteSubject subject = (ConcreteSubject)arg0.getBean("concreteSubject");
-		System.out.println("setBeanFactory. Subject = " + subject.toString());
-		
 	}
 
 }
